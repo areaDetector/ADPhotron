186aa8a09bdf0fb2e94f446ae12746b4a10e1967
Kevin Peterson <kmpeters@anl.gov>
2015-12-21 14:02:47 -0600

    Added batch file to edit Photron adl

---
+++ iocs/photronIOC/iocBoot/iocPhotron/edit_medm.bat
@@ -0,0 +1,16 @@
+@echo ON
+
+SETLOCAL
+
+set EPICS_DISPLAY_PATH=D:\epics\AD-2-4\areaDetector-R2-4\ADPhotron\photronApp\op\adl
+set EPICS_DISPLAY_PATH=%EPICS_DISPLAY_PATH%;D:\epics\AD-2-4\areaDetector-R2-4\ADCore-R2-4\ADApp\op\adl
+set EPICS_DISPLAY_PATH=%EPICS_DISPLAY_PATH%;D:\epics\synApps\support\asyn-4-27\opi\medm
+
+set PATH=%PATH%;C:\Program Files\EPICS Windows Tools
+
+echo %EPICS_DISPLAY_PATH%
+
+REM launch medm
+start medm PhotronDevel.adl
+
+ENDLOCAL
