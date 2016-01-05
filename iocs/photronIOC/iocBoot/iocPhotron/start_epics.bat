3ec7a747b1314ad9f2ccfb91b1c73b750f33dc34
Kevin Peterson <kmpeters@anl.gov>
2016-01-05 14:01:23 -0600

    Workaround for caQtDM putting conflicting dlls on the PATH.

--- iocs/photronIOC/iocBoot/iocPhotron/start_epics.bat
+++ iocs/photronIOC/iocBoot/iocPhotron/start_epics.bat
@@ -10,6 +10,9 @@ set EPICS_HOST_ARCH=windows-x64-debug
 REM dlls to the PATH
 call dllPath.bat
 
+REM caQtDM puts conflicting dlls on the PATH
+set PATH=D:/epics/base-3.14.12.5/bin/windows-x64-debug;%PATH%
+
 REM start the IOC
 ..\..\bin\%EPICS_HOST_ARCH%\photronApp st.cmd
 
