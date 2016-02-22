fec5e775e80d52d63c8669178c75a643c59dd976
Kevin Peterson <kmpeters@anl.gov>
2016-02-22 15:42:21 -0600

    Added EPICS_DISPLAY_PATH to edit_adl.bat

--- photronApp/op/adl/edit_adl.bat
+++ photronApp/op/adl/edit_adl.bat
@@ -2,6 +2,10 @@
 
 SETLOCAL
 
+set EPICS_DISPLAY_PATH=D:\epics\AD-2-4\areaDetector-R2-4\ADPhotron\photronApp\op\adl
+set EPICS_DISPLAY_PATH=%EPICS_DISPLAY_PATH%;D:\epics\AD-2-4\areaDetector-R2-4\ADCore-R2-4\ADApp\op\adl
+set EPICS_DISPLAY_PATH=%EPICS_DISPLAY_PATH%;D:\epics\synApps\support\asyn-4-27\opi\medm
+
 set PATH=%PATH%;C:\Program Files\EPICS Windows Tools
 
 echo %EPICS_DISPLAY_PATH%
