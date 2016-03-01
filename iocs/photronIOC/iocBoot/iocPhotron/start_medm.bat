dc801dd4224cd684712cb79e486eee8f89529d65
Kevin Peterson <kmpeters@anl.gov>
2016-03-01 14:36:16 -0600

    Made calcs for trigger reset available from the "more" menu

--- iocs/photronIOC/iocBoot/iocPhotron/start_medm.bat
+++ iocs/photronIOC/iocBoot/iocPhotron/start_medm.bat
@@ -6,6 +6,7 @@ REM The following paths will need to be customized when the IOC is deployed
 set EPICS_DISPLAY_PATH=D:\epics\AD-2-4\areaDetector-R2-4\ADPhotron\photronApp\op\adl
 set EPICS_DISPLAY_PATH=%EPICS_DISPLAY_PATH%;D:\epics\AD-2-4\areaDetector-R2-4\ADCore-R2-4\ADApp\op\adl
 set EPICS_DISPLAY_PATH=%EPICS_DISPLAY_PATH%;D:\epics\synApps\support\asyn-4-27\opi\medm
+set EPICS_DISPLAY_PATH=%EPICS_DISPLAY_PATH%;D:\epics\synApps\support\calc-3-4-2\calcApp\op\adl
 
 set PATH=%PATH%;C:\Program Files\EPICS Windows Tools
 
